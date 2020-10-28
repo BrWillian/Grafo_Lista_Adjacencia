cd0b0a5d38034e81bd7b462ef4bcd867ad491319
willian <wiliam-m-@hotmail.com>
2020-10-28 01:29:51 -0300

    commit initial

---
+++ grafo.h
@@ -0,0 +1,18 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+#include <list>
+#include <string>
+
+using namespace std;
+
+class grafo
+{
+private:
+    int V;
+    list<int> * adj;
+
+public:
+    grafo();
+};
+
+#endif // GRAFO_H

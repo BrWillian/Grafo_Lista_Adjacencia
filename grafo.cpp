47308da88e0ba633b96c4fb43dfaff0e5a1608d0
willian <wiliam-m-@hotmail.com>
2020-10-28 01:41:18 -0300

    updated graph estructure

--- grafo.cpp
+++ grafo.cpp
@@ -1,6 +1,28 @@
 #include "grafo.h"
+#include <algorithm>
 
-grafo::grafo()
+Grafo::Grafo(int V)
 {
-
+    this->V = V;  // atribui o numero de vertices
+    adj = new list<int>[V]; // cria a lista de adjacencias
 }
+void Grafo::adicionarAresta(int v1, int v2)
+{
+    // adicionar vertice v2 a lista de vertices adjacentes de v1
+    adj[v1].push_back(v2);
+}
+int Grafo::obterGrau(int v)
+{
+    // tamanho da lista que e a quantidade de vizinhos
+    return adj[v].size();
+}
+bool Grafo::existeVizinho(int v1, int v2)
+{
+    // procura o vizinho na lista de adjencia
+    if(find(adj[v1].begin(), adj[v1].end(), v2) != adj[v1].end())
+    {
+        return true;
+    }
+    return false;
+}
+
--- grafo.h
+++ grafo.h
@@ -2,17 +2,24 @@
 #define GRAFO_H
 #include <list>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-class grafo
+class Grafo
 {
 private:
     int V;
     list<int> * adj;
 
 public:
-    grafo();
+    Grafo(int V);
+    void adicionarAresta(int v1, int v2);
+    int obterGrau(int v);
+
+    bool existeVizinho(int v1, int v2);
+    vector<int>listarVizinhos(int v);
+
 };
 
 #endif // GRAFO_H
